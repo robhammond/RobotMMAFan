7aeb4f7cc2da0f643e313c2f1a90f26d9b15fe9c
Eduardo Arino de la Rubia <earino@gmail.com>
2013-05-23 03:04:45 +0400

    tagged more

--- scripts/main.pl
+++ scripts/main.pl
@@ -23,7 +23,9 @@ my $nt = Net::Twitter->new(
 my $URL = 'http://api.twitter.com/1/lists/statuses.json?slug=fighters&owner_screen_name=ufc';
 my $latest_id = undef;
 my @bad_words = qw/
-  fag homo gay queer faggot tranny 
+  fag faggot fagget homo gay queer tranny rape
+  cunt lesbian lesbo nigger whore motherfucker
+  cocksucker twat retard fucktard paki
 /;
 
 while (1) {
@@ -31,16 +33,25 @@ while (1) {
   if ($latest_id) {
     $current_url .= "&since_id=$latest_id";
   }
-  my $content = get($current_url);
-  die "Couldn't get it, reason: $!" unless defined $content;
+  my $content;
+
+  {
+	$content = get($current_url);
+	sleep 60 and redo unless defined $content;
+  }
   my $data = decode_json($content);
 
   foreach my $tweet (reverse @{$data}) {
     $latest_id = $tweet->{id};
     foreach my $bad_word (@bad_words) {
-      if ($tweet->{text} =~ qr/$bad_word/) {
-        my $result = $nt->update("\"@".$tweet->{user}->{screen_name}." ".$tweet->{text}."\n");
-        $result = $nt->reteweet($tweet->{id});
+      if ($tweet->{text} =~ qr/\b$bad_word\b/i) {
+	my $o_content = "\"@".$tweet->{user}->{screen_name}." ".$tweet->{text}."\"";
+	$content = substr($o_content, 0, 140);
+
+	print "Matched: $bad_word\n";
+	print "Retweeting: $content was $o_content ";
+        my $result = $nt->update($content);
+	print "Result: ".Dumper($result)."\n";
       }
     }
   }
